4f00eda0bf77bf3a27c26bb58bc642ffe576a5fc
Paul Baxter <jareth_2005@yahoo.com>
2026-07-15 10:18:21 -0400

    Add files via upload

--- buildparsetree/main.cpp
+++ buildparsetree/main.cpp
@@ -14,20 +14,21 @@
 #include <stdexcept>
 #include <string_view>
 #include <utility>
-#include <stdexcept>
 #include <charconv>
 
 #include "Tokenizer.h"
 #include "CsvReader.h"
 
-
 int main(int argc, char* argv[])
 {
 	if (argc != 3) {
-		std::clog << "This builds a Trie state machine parser from a CSV file of keywords and tokens.\n\n"
-		          << "\n\nThe user can then call:\n"
-		          << "static MatchResult match_longest_token(const std::string & text, size_t start_pos)\n"
-		          << "Usage: " << argv[0] << " <inputfile.csv> <outputfile.h>\n";
+		std::clog
+			<< "This builds a Trie state machine parser from a CSV file of keywords and tokens.\n"
+			<< "Usage: " << argv[0] << " <inputfile.csv> <outputfile.h>\n"
+			<< "\n\nThe user can then call:\n"
+			<< "static MatchResult match_longest_token(const std::string & text, size_t start_pos)\n"
+			;
+		return 1;
 	}
 
     std::map<std::string, int> keywordToToken;
